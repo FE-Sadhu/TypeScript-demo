bba79f56a4aedeb19c2341fe13148af90700312f
sadhu <1714222385@qq.com>
2019-12-20 17:57:01 +0800

    泛型接口

---
+++ catTs/进阶/泛型/泛型接口.ts
@@ -0,0 +1,36 @@
+/**
+ * 怎样定义泛型接口
+ */
+
+ // 方式一 -> 接口里面定义泛型函数
+ interface CreateArrayFunc {
+   <T>(length: number, value: T): Array<T>
+ }
+ let createArray: CreateArrayFunc;
+ createArray = function<T>(length: number, value: T): Array<T> {
+    let result: T[] = [];
+    for (let i = 0; i < length; i++) {
+        result[i] = value;
+    }
+    return result;
+ }
+
+ createArray(3, 'x'); // ['x', 'x', 'x']
+
+ // 方式二 -> 定义泛型接口
+ interface CreateArrayFunc1<T> {
+   (length: number, value: T): Array<T>;
+ }
+
+ let createArray1: CreateArrayFunc1<any>; // 一定注意，在使用泛型接口的时候，需要定义泛型的类型。
+ createArray1 = function<T>(length: number, value: T): Array<T> {
+   const result: T[] = [];
+
+   for (let i = 0; i < length; i++) {
+     result[i] = value;
+   }
+
+   return result;
+ }
+
+ createArray1(3, 'x'); // ['x', 'x', 'x']
